84a6a7bd61c953df450bb96fe9b4d3c835110f1e
Ondra Janoška <o.janoska98@gmail.com>
2024-11-13 10:18:14 +0100

    moving shit around for no good reason

--- api/homeworks/compile.sh
+++ api/homeworks/compile.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+echo "Compiling homeworks..."
 FILES=$(ls */*.c)
 for file in $FILES
 do
 	outName=$(echo "$file" | cut -d'.' -f1)
 	gcc -O2 -o "$outName".out "$file"
 done
+echo "Homeworks compiled."
